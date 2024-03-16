f2a65929ac622e58b30d9f64c92b569d435032aa
Mike <tutoruialsspartan@gmail.com>
2024-03-16 17:54:31 -0600

    Base de datos y pruebas

--- Datos/Consultas.cs
+++ Datos/Consultas.cs
@@ -21,7 +21,7 @@ class Consulta : DatosCli
         }
     }
     
-    public void Prueba()
+    public void Prueba() //Parte para visualizar datos
     {
         string query = "SELECT * FROM Datos_usuario";
 
@@ -32,16 +32,48 @@ class Consulta : DatosCli
             conexion.Open();
             using(SQLiteCommand comando = new SQLiteCommand(query, conexion))
             {
+                    Console.WriteLine("Datos de la tabla Datos_usuario");
+                    Console.WriteLine("|ID\t|Nombre                   |Apellido Paterno    |Apellido Materno    |");
                    using(SQLiteDataReader reader = comando.ExecuteReader())
                     {
                         while(reader.Read())
                         {
-                            Console.WriteLine($"ID: {reader["ID"]}, Nombre: {reader["Nombre"]}, Apellido: {reader["Apellido_paterno"]}");                  
-                        }
+                        Console.WriteLine($"|{reader["ID"]}\t|{reader["Nombre"].ToString().PadRight(25)}|{reader["Apellido_paterno"].ToString().PadRight(20)}|{reader["Apellido_materno"].ToString().PadRight(20)}|");                        }
                     }
             
             }
+
+            string query2 = "SELECT * FROM Tipo_sangre";
+            using(SQLiteCommand comando2 = new SQLiteCommand(query2, conexion))
+            {
+                Console.WriteLine("Datos de la tabla Tipo_sangre");
+                Console.WriteLine("|ID   |Factor RH |Tipo sangre|Estatus        |Observaciones                 |");
+
+                using(SQLiteDataReader reader = comando2.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Console.WriteLine($"|{reader["id"].ToString().PadRight(5)}|{reader["factor_rh"].ToString().PadRight(10)}|{reader["tipo_sangre"].ToString().PadRight(11)}|{reader["estatus"].ToString().PadRight(15)}|{reader["Observaciones"].ToString().PadRight(30)}|");
+                        }
+                    }
+            }
+            string query3 = "SELECT * FROM Solicitudes";
+            using(SQLiteCommand comando2 = new SQLiteCommand(query3, conexion))
+            {
+                Console.WriteLine("Datos de la tabla solicitudes");
+                Console.WriteLine("|ID   |ID Solicitante|Fecha     |");
+
+                using(SQLiteDataReader reader = comando2.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Console.WriteLine($"|{reader["id"].ToString().PadRight(5)}|{reader["id_solicitante"].ToString().PadRight(14)}|{reader["fecha"].ToString().PadRight(10)}|");                        
+                        }
+                    }
+            }
         }
+
+
     }
 
 }
